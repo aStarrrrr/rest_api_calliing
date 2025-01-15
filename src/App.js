bcc0af2760fa3c3d3850411020e29c97b222ecf5
Abhinand Shaji <abhinandshaji369@gmail.com>
2025-01-15 09:43:07 +0530

    Just get the data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
   return (
     <div>
       <h1>Abhinand Shaji</h1>
+      <button onClick={()=>{
+        axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+          console.log(response)
+        })
+      }} >Click me</button>
     </div>
   );
 }

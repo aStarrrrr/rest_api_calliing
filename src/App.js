6c890897f0f14e54de261010803adce9c7ea5570
Abhinand Shaji <abhinandshaji369@gmail.com>
2025-01-15 09:54:29 +0530

    Data collected and displayed by calling web service (axios)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 // import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
+import { useState } from 'react';
 
 function App() {
+  const [state,setState] = useState([])
   return (
     <div>
       <h1>Abhinand Shaji</h1>
       <button onClick={()=>{
         axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
           console.log(response)
+          setState(response.data)
         })
       }} >Click me</button>
+      {state.map((obj,index)=>{
+        return(
+          <div>
+            <h1>{index + 1}</h1>
+            <h1>{obj.title}</h1>
+            <h4>{obj.body}</h4>
+          </div>
+        )
+      })}
     </div>
   );
 }
